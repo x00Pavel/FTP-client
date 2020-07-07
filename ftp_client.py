89b0792bd175adc8436ec0afb504f51fb3108b42
x00Pavel <xyadlo00@stud.fit.vutb.cz>
2020-07-07 23:17:17 +0200

    Imporvement of code, add more fucntionality

--- ftp_client.py
+++ ftp_client.py
@@ -4,6 +4,17 @@ import os
 from builtins import open
 
 USER_NAME = os.getlogin()
+CMD = ["cd", "mkdir", "upload", "download", "os_ls", "ftp_ls", "help"]
+
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 class Host:
     def __init__(self):
@@ -13,31 +24,36 @@ class Host:
             host = user_input("Set FTP server: ")
             if host == "":
                 host = "192.168.0.81"
+            
+            try:
+                self.host = FTP(host)
+                break
+            except Exception as e:
+                print(e)
+                info(["FTP server is not responsing, try again"])
+                continue
+
+        while True:
             user = user_input("Login: ")
             if user == "":
                 user = "sammy"
             psswd = user_input("Password: ")
             if psswd == "":
                 psswd = "1234"
-            
+                        
             try:
-                self.host = FTPHost(host, user, psswd)
+                self.host.login(user, psswd)
                 break
-            except:
-                info(["FTP server is not responsing, try again"])
+            except Exception as e:
+                print(e)
+                info(["Wrong user info. Try again..."])
                 continue
-
-        self.actions = {"1": self.upload, 
-               "upload": self.upload, 
-               "2": self.download, 
-               "download": self.download,
-               "ftp_cd": self.change_dir}
-            #    TODO commands
+        info([self.host.getwelcome()])
 
     def change_dir(self, path, host="ftp"):
         try:
             if host == "ftp":
-                self.host.chdir(path)
+                self.host.cwd(path)
             else:
                 os.chdir(path)
         except:
@@ -46,19 +62,46 @@ class Host:
     
     def show_dir(self, host="ftp"):
         if host == "ftp":
-            self.host.listdir(self.host.curdir)
+            self.host.nlst(self.host.pwd())
         else:
             info(os.listdir())
+
+    def create_dir(self, name):
+        self.host.mkd(name)
 
     def download(self):
         info(["Choose: download"])
         # TODO downloading
 
     def upload(self):
-        # self.show_dir()
-        file = user_input("Choose file or directory: ")
-        # TODO uploading
-
+        src = user_input("Choose file or directory: ")
+        if os.path.exists(os.path.abspath(src)):
+            dst = user_input(f"Destination (default {src}): ")
+            cur_dir = self.host.pwd() # save direcotry to return after uploading
+            if dst == "":
+                dst = src
+            else:
+                path_parts = dst.split("/")
+                for part in path_parts[0:-1:1]:
+                    if part not in self.host.nlst():
+                        print_error(2, "/".join(path_parts[path_parts.index(part)::]))
+                        answer = user_input("Do you want to create this folder? [yes(default)|no] ")
+                        if answer == "yes":
+                            self.create_dir(part)
+                            self.change_dir(part)
+                        elif answer == "no":
+                            continue
+                    else:
+                        self.change_dir(part)
+                
+            if os.path.isdir(src):
+                pass
+            elif os.path.isfile(src):
+                with open(src, "rb") as source:
+                    self.host.storbinary(f"STOR {dst}", source)
+                self.change_dir(cur_dir)
+        else:
+            print_error(2, src)
 
 class Session:
 
@@ -67,13 +110,17 @@ class Session:
             try:
                 os.chdir(f"/home/{USER_NAME}/")
                 self.ftp_host = Host()
-            except:
+                break
+            except Exception as e:
+                info([e])
                 info(["There is problem with loging. Try again..."])
                 continue
 
         self.keep_alive = True
-
     
+    def close_session(self):
+        self.keep_alive = False
+
 def info(args=[""]):
     for item in args:
         print(f"ftp > {item}")
@@ -83,36 +130,53 @@ def user_input(string):
     return input(f"ftp > {string}")
 
 
-def choose_action(host):
-    info(["Choose action:", "\t1 - Upload", "\t2 - Download"])
-    while True:
-        action = user_input("")
-        if action.lower() not in host.actions.keys():
-            info(["Wrong command. Try again..."])
-        else:
-            return host.actions[action]
-
-
-def print_cmd():
-    info(["Commands:",
+def print_help():
+    info([Colors.BOLD + "Commands:" + Colors.ENDC,
+        "\thelp - print this help message",
         "\tcd <path> - change directory",
         "\tmkdir <path> - create directory",
         "\trm <path> - remove file",
+        "\tos_ls - list current directory on system",
+        "\tftp_ls - list current directory on ftp server",
+        "\tupload - to start uploading process",
+        "\tdownload - to start downloading process",
         "This commands similar in meaning to standart bash commands and can be inserted in any time of processing"])
-        # TODO commands
+        # TODO commands 
 
+
+def print_error(code=0, *args):
+    args = ": {}".format(" ".join(args)) if args is not None else ""
+    if code == 1:
+        print(Colors.FAIL + "Command do not exist" + args + Colors.ENDC)
+    elif code == 2:
+        print(Colors.FAIL + "Directory does not exist"  + args + Colors.ENDC)
+
+def parse_input(session):
+        answer = user_input("").lower()
+        print(answer)
+        if answer not in CMD:
+            print_error(1)
+        elif answer == "help":
+            print_help()
+        elif answer == "upload":
+            session.ftp_host.upload()
+        elif answer == "download":
+            session.ftp_host.download()
+        elif answer == "exit":
+            session.close_session()
 
 def main():
-    session = Session()
-    print_cmd()
-    while session.keep_alive:
+    try:
+        session = Session()
+        print_help()
+        while session.keep_alive:
+            parse_input(session)
+    except KeyboardInterrupt:
+        info([Colors.FAIL + "Force exit..." + Colors.ENDC])
+    except Exception as e:
+        info([e])
+    finally:
+        session.ftp_host.host.close()
         
-        action = choose_action(session.ftp_host)
-        try:
-            action()
-        except Exception as e:
-            info([e])
-            break
-
 if __name__ == "__main__":
     main()
